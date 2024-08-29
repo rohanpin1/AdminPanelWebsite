8bda486b8b0328a5d4a831b8d3aad870ead51828
Parul <108892371+rohanpin1@users.noreply.github.com>
2024-08-29 13:55:42 +0530

    Add Login And Register Page

--- AdminWeb/AdminWeb/Program.cs
+++ AdminWeb/AdminWeb/Program.cs
@@ -26,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
